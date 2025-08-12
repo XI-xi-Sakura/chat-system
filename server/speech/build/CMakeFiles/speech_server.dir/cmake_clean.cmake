477f7fcf8f96dba35bb1d27fc2e2c77a46642162
XI-xi-Sakura <1959701844@qq.com>
2025-08-12 11:16:16 +0800

    build_test

---
+++ server/speech/build/CMakeFiles/speech_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/speech_server.dir/source/speech_server.cc.o"
+  "CMakeFiles/speech_server.dir/speech.pb.cc.o"
+  "speech.pb.cc"
+  "speech_server"
+  "speech_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/speech_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
