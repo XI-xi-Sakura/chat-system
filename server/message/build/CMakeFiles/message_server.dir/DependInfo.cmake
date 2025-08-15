a5d20cc4a8166f91c62744ca94f63779a510e6d4
XI-xi-Sakura <1959701844@qq.com>
2025-08-15 12:00:14 +0800

    message_server

---
+++ server/message/build/CMakeFiles/message_server.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/chat-system/server/message/build/base.pb.cc" "/home/ubuntu/chat-system/server/message/build/CMakeFiles/message_server.dir/base.pb.cc.o"
+  "/home/ubuntu/chat-system/server/message/build/file.pb.cc" "/home/ubuntu/chat-system/server/message/build/CMakeFiles/message_server.dir/file.pb.cc.o"
+  "/home/ubuntu/chat-system/server/message/build/message-odb.cxx" "/home/ubuntu/chat-system/server/message/build/CMakeFiles/message_server.dir/message-odb.cxx.o"
+  "/home/ubuntu/chat-system/server/message/build/message.pb.cc" "/home/ubuntu/chat-system/server/message/build/CMakeFiles/message_server.dir/message.pb.cc.o"
+  "/home/ubuntu/chat-system/server/message/source/message_server.cc" "/home/ubuntu/chat-system/server/message/build/CMakeFiles/message_server.dir/source/message_server.cc.o"
+  "/home/ubuntu/chat-system/server/message/build/user.pb.cc" "/home/ubuntu/chat-system/server/message/build/CMakeFiles/message_server.dir/user.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../common"
+  "../../odb"
+  "../../third/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/message/build/CMakeFiles/message_server.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/message_server.dir/base.pb.cc.o"
+  "CMakeFiles/message_server.dir/file.pb.cc.o"
+  "CMakeFiles/message_server.dir/message-odb.cxx.o"
+  "CMakeFiles/message_server.dir/message.pb.cc.o"
+  "CMakeFiles/message_server.dir/source/message_server.cc.o"
+  "CMakeFiles/message_server.dir/user.pb.cc.o"
+  "base.pb.cc"
+  "file.pb.cc"
+  "message-odb.cxx"
+  "message.pb.cc"
+  "message_server"
+  "message_server.pdb"
+  "user.pb.cc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/message_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
