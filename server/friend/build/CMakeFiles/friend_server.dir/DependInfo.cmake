005ee2ce92c60bc7b22babe2d617b78c36ac3a4d
XI-xi-Sakura <1959701844@qq.com>
2025-08-17 12:40:18 +0800

    friend_server

---
+++ server/friend/build/CMakeFiles/friend_server.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/chat-system/server/friend/build/base.pb.cc" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/base.pb.cc.o"
+  "/home/ubuntu/chat-system/server/friend/build/chat_session-odb.cxx" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/chat_session-odb.cxx.o"
+  "/home/ubuntu/chat-system/server/friend/build/chat_session_member-odb.cxx" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/chat_session_member-odb.cxx.o"
+  "/home/ubuntu/chat-system/server/friend/build/friend.pb.cc" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/friend.pb.cc.o"
+  "/home/ubuntu/chat-system/server/friend/build/friend_apply-odb.cxx" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/friend_apply-odb.cxx.o"
+  "/home/ubuntu/chat-system/server/friend/build/message.pb.cc" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/message.pb.cc.o"
+  "/home/ubuntu/chat-system/server/friend/build/relation-odb.cxx" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/relation-odb.cxx.o"
+  "/home/ubuntu/chat-system/server/friend/source/friend_scrver.cc" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/source/friend_scrver.cc.o"
+  "/home/ubuntu/chat-system/server/friend/build/user.pb.cc" "/home/ubuntu/chat-system/server/friend/build/CMakeFiles/friend_server.dir/user.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../common"
+  "../../odb"
+  "../../third/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/friend/build/CMakeFiles/friend_server.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/friend_server.dir/base.pb.cc.o"
+  "CMakeFiles/friend_server.dir/chat_session-odb.cxx.o"
+  "CMakeFiles/friend_server.dir/chat_session_member-odb.cxx.o"
+  "CMakeFiles/friend_server.dir/friend.pb.cc.o"
+  "CMakeFiles/friend_server.dir/friend_apply-odb.cxx.o"
+  "CMakeFiles/friend_server.dir/message.pb.cc.o"
+  "CMakeFiles/friend_server.dir/relation-odb.cxx.o"
+  "CMakeFiles/friend_server.dir/source/friend_scrver.cc.o"
+  "CMakeFiles/friend_server.dir/user.pb.cc.o"
+  "base.pb.cc"
+  "chat_session-odb.cxx"
+  "chat_session_member-odb.cxx"
+  "friend.pb.cc"
+  "friend_apply-odb.cxx"
+  "friend_server"
+  "friend_server.pdb"
+  "message.pb.cc"
+  "relation-odb.cxx"
+  "user.pb.cc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/friend_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
