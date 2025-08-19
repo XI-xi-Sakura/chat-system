35635fe41c586c84d1dcd285b99ae6510408439f
XI-xi-Sakura <1959701844@qq.com>
2025-08-19 19:16:13 +0800

    gateway

---
+++ server/gateway/test/CMakeFiles/gateway_server.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/chat-system/server/gateway/test/base.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/base.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/file.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/file.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/friend.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/friend.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/gateway.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/gateway.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/message.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/message.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/notify.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/notify.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/source/gateway_server.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/source/gateway_server.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/speech.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/speech.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/transmite.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/transmite.pb.cc.o"
+  "/home/ubuntu/chat-system/server/gateway/test/user.pb.cc" "/home/ubuntu/chat-system/server/gateway/test/CMakeFiles/gateway_server.dir/user.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../common"
+  "../../third/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
